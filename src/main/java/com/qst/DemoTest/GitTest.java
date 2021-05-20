153adf81d91dbbf968eb8716fad4c0ea318fc37f
Jicaiming <1261128518@qq.com>
2021-05-20 11:34:46 +0800

    hot-fix commit2

--- src/main/java/com/qst/DemoTest/GitTest.java
+++ src/main/java/com/qst/DemoTest/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("git test");
         System.out.println("git test2");
         System.out.println("git test3");
-        System.out.println("hot-fix 进行了热修复");
+        System.out.println("hot-fix test");
     }
 }
