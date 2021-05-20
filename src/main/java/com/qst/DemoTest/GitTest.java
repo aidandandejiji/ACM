e6018434fa9d7a770011e70fc79ed1526c2f6f1b
Jicaiming <1261128518@qq.com>
2021-05-20 11:10:17 +0800

    third commit

--- src/main/java/com/qst/DemoTest/GitTest.java
+++ src/main/java/com/qst/DemoTest/GitTest.java
@@ -3,5 +3,7 @@ package com.qst.DemoTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test2");
+        System.out.println("git test3");
     }
 }
